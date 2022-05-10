302ea837ee681641e6c121d87dbebf9d7f3aee57
EnjoyW0rld <denisavatar@hotmail.com>
2022-05-10 18:33:46 +0200

    screen loading animation

--- GXPEngine/MyGame.cs
+++ GXPEngine/MyGame.cs
@@ -7,8 +7,15 @@ using System.IO;
 public class MyGame : Game
 {
 	List<Scene> scenes = new List<Scene> ();
+	EasyDraw transition;
+	bool isLoading;
+	int nextLevel;
+	Tween screenCover;
 	public MyGame() : base(1920, 1080, false,false)		// Create a window that's 800x600 and NOT fullscreen
 	{
+		transition = new EasyDraw(1920, 1080,false);
+		transition.Clear(0);
+		transition.x = -1920;
 		//Scene sc = new Scene("level1.tmx");
 		targetFps = 60;
         foreach (string item in Directory.GetFiles("Assets/Levels/"))
@@ -19,22 +26,43 @@ public class MyGame : Game
         //AddChild(scenes[0]);
 
         AddChild(scenes[0]);
-        
-		EventsHandler.LevelChange += ChangeTo;
+		EventsHandler.LevelChange += StartLoading;
+		//EventsHandler.LevelChange += ChangeTo;
+		AddChild(transition);
 	}
 
 	// For every game object, Update is called every frame, by the engine:
 	void Update()
 	{
+
 		//Gizmos.DrawLine(,null,255);
 		// Empty
+		if (isLoading && !transition.HasChild(screenCover))
+		{
+			ChangeTo(nextLevel); 
+			isLoading = false;
+		}
 	}
 	void ChangeTo(int n)
     {
-		foreach(var item in GetChildren()) RemoveChild(item);
-		if(scenes.Count + 1 >= n + 1) AddChild(scenes[n]);
+		foreach (var item in GetChildren())
+		{
+			if(!(item is EasyDraw))
+			RemoveChild(item);
+		}
+		if(scenes.Count + 1 >= n + 1) AddChildAt(scenes[n],0);
         Console.WriteLine("levelChanged");
+		//EventsHandler.OpenTransition?.Invoke();
+		transition.AddChild(new Tween(Tween.Parameter.x, 1, -1920, Tween.Function.easeInQuad));
     }
+
+	void StartLoading(int i)
+    {
+		nextLevel = i;
+		if(!transition.HasChild(screenCover))
+		transition.AddChild(screenCover = new Tween(Tween.Parameter.x, 1, 0, Tween.Function.easeInQuad));
+		isLoading = true;
+    }	
 
 	static void Main()							// Main() is the first method that's called when the program is run
 	{
